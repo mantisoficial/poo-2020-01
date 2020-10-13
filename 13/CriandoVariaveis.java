61a76bf3e1116f55e66098007dd94b788cc98cb5
Arthur <54213043+mantisoficial@users.noreply.github.com>
2020-10-13 15:04:03 -0300

    Add files via upload

---
+++ 13/CriandoVariaveis.java
@@ -0,0 +1,13 @@
+// criando variávies de tipos diferentes
+public class CriandoVariaveis {
+    public static void main(String[] args) {
+        int x = 12;
+        float y = 15.4f;
+        double z = 23.589d;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+    }
+}
